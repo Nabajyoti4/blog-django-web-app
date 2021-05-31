b3a425c54665458b31fc93d07c5084ec6f152b4a
Nabajyoti <sirnaba@gmail.com>
2021-05-31 22:00:01 +0530

    New base html page added in global and index html page added in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 
-urlspattern = [
+urlpatterns = [
     path("", views.index, name="index"),
     path("posts", views.posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
