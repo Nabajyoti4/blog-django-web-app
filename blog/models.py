3383831439f7ca09971115e767718dda2176258f
Nabajyoti <sirnaba@gmail.com>
2021-06-02 21:07:20 +0530

    Data added in all models using admin panel, displayes top 3 post in index page from database

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django.db.models.fields import CharField
+from django.utils import tree
 
 # Create your models here.
 class Tag(models.Model):
     caption = CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return f"{self.caption}"
+
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
 
 
 class Post(models.Model):
@@ -19,5 +29,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL,null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+
+    
